97675df70d452061840b3150ac57140253d5ce78
Ericson Joseph <ericsonjoseph@gmail.com>
2018-08-20 11:05:52 -0500

    Release 1.0

--- src/main.c
+++ src/main.c
@@ -215,7 +215,7 @@ void SystemClock_Config(void) {
 static void MX_USART3_UART_Init(void) {
 
 	huart3.Instance = USART3;
-	huart3.Init.BaudRate = 576000;
+	huart3.Init.BaudRate = 921600;
 	huart3.Init.WordLength = UART_WORDLENGTH_8B;
 	huart3.Init.StopBits = UART_STOPBITS_1;
 	huart3.Init.Parity = UART_PARITY_NONE;
